d52a1d135cc759d23a288652decaef85f48b3080
Michael Griscom <michaelgriscom@gmail.com>
2018-06-21 15:50:24 +0200

    showKeyGuide setting (#21)
    * adding setting
    
    * wip
    
    * cleanup
    
    * fixes
    
    * Updating readme with showKeyGuide setting

--- src/strings.ts
+++ src/strings.ts
@@ -3,4 +3,10 @@ export const keybindingsSetting: string = "keybindings";
 export const isActiveSetting: string = `${extensionName}.isActive`;
 export const enterLeaderModeCommand: string = `${extensionName}.enter`;
 export const exitLeaderModeCommand: string = `${extensionName}.exit`;
+export const showKeyGuideSetting: string = `showKeyGuide`;
+export enum ShowKeyGuide {
+    Always = "always",
+    Never = "never"
+}
+
 export const noLabel: string = "No Description";
