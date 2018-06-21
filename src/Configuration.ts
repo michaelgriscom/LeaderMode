e9968b703a1578f3b6195739ade79b3764e1d7ca
Michael Griscom <migris@microsoft.com>
2018-06-21 10:29:22 +0200

    adding setting

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -1,5 +1,5 @@
 import { workspace } from "vscode";
-import { extensionName, keybindingsSetting } from "./strings";
+import { extensionName, keybindingsSetting, ShowKeyGuide, showKeyGuideSetting } from "./strings";
 
 export interface IKeybinding {
     keySequence: string[];
@@ -10,12 +10,14 @@ export interface IKeybinding {
 
 export interface IConfiguration {
     keybindings: IKeybinding[];
+    showKeyGuide: ShowKeyGuide;
 }
 
 export function loadConfiguration(): IConfiguration {
     const WorkspaceConfiguration = workspace.getConfiguration(extensionName);
     const config: IConfiguration = {
         keybindings: WorkspaceConfiguration.get<IKeybinding[]>(keybindingsSetting) || [],
+        showKeyGuide: WorkspaceConfiguration.get<ShowKeyGuide>(showKeyGuideSetting) || ShowKeyGuide.Always,
     };
 
     return config;
